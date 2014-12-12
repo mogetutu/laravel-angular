5955618d42f4fdb4e33bb1820b1e1624ba3546cc
mogetutu <imogetutu@gmail.com>
2014-12-12 13:53:20 +0300

    updated Todo model

--- app/Acme/Todo.php
+++ app/Acme/Todo.php
@@ -3,5 +3,7 @@
 class Todo extends \Eloquent {
 
     protected $table = 'todos';
+    protected $fillable   = array('title', 'description', 'status');
+    protected $guarded    = array('id');
 
 }
