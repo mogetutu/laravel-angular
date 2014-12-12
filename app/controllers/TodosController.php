939e0b4f84cf352cb789a9113cb696834e5eecdb
mogetutu <imogetutu@gmail.com>
2014-12-12 16:44:04 +0300

    added index and show methods

--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -1,12 +1,45 @@
 <?php
 
 use Acme\Todo;
+use Illuminate\Support\Facades\Response;
 
 class TodosController extends BaseController {
 
+    protected $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
+    /**
+     * All todos
+     * @return json
+     */
     public function index()
     {
-        return Todo::all();
+        return Response::json($this->todo->all());
+    }
+
+    public function store()
+    {
+        $validator = Validator::make(Input::all(), $this->todo->rules());
+        if ($validator->fails()){
+            return Response::json($validator->messages(), 404);
+        }
+        $todo = $this->todo->create(['title' => Input::get('title')]);
+
+        return Response::json($todo, 201);
+    }
+
+    public function show($id)
+    {
+        $todo = $this->todo->find($id);
+        if ($todo) {
+            return Response::json($todo);
+        }
+
+        return Response::json(['error' => 'Resource Not Found.'], 404);
     }
 
 }
