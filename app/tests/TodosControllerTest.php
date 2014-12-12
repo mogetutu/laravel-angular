ec431e5a879a6bcb78b2fc42260841555eb871eb
mogetutu <imogetutu@gmail.com>
2014-12-12 18:06:23 +0300

    added mocking to tests

--- app/tests/TodosControllerTest.php
+++ app/tests/TodosControllerTest.php
@@ -1,54 +1,94 @@
 <?php
 
-use Acme\Todo;
-
 class TodosControllerTest extends TestCase {
-
-    public function testBaseUrl()
+    public function setUp()
     {
-        $crawler = $this->client->request('GET', '/');
-        $this->assertTrue($this->client->getResponse()->isOk());
+        parent::setUp();
+        $this->todo = [
+            'id'        => 1,
+            'title'     => 'New Todo Item',
+            'completed' => 0
+        ];
+        $this->mock = Mockery::mock('Eloquent', '\Acme\Todo');
     }
 
-    public function testGetTodos()
+    public function tearDown()
     {
-        $response = $this->call('GET', '/todos');
+        Mockery::close();
+    }
 
-        $this->assertEquals(200, $response->getStatusCode());
-        $data = json_decode($response->getContent());
-        $this->assertInternalType('array', $data, 'Invalid JSON');
+    public function testGet()
+    {
+        $todo = array($this->todo);
+        $this->mock->shouldReceive('all')->once()->andReturn($todo);
+        $this->app->instance('\Acme\Todo', $this->mock);
+        $request = $this->call('GET', '/todos');
+        $this->assertEquals(200, $request->getStatusCode());
+        $this->assertEquals($request->getContent(), $todo);
     }
 
     public function testCreateTodo()
     {
-        $lastTodo = Todo::orderBy('created_at', 'DESC')->first();
-        $todo     = ['title' => 'Another Todo'];
-        $request  = $this->call('POST', '/todos', $todo);
+        $this->mock = Mockery::mock('Eloquent', '\Acme\Todo');
+        $this->mock->shouldReceive('create')->once()->andReturn($this->todo);
+        $this->app->instance('\Acme\Todo', $this->mock);
+        $request = $this->call('POST', '/todos', $this->todo);
         $this->assertEquals(201, $request->getStatusCode());
-
-        $response = $this->call('GET', '/todos/' . $lastTodo->id);
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals($response->getContent(), $lastTodo);
+        $this->assertEquals($request->getContent(), '{"id":1,"title":"New Todo Item","completed":0}}');
     }
 
     public function testGetByExistingId()
     {
-        $response = $this->call('GET', '/todos/1');
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals($response->getContent(), '{"id":1,"title":"Create Laravel App","completed":1,"created_at":"2014-12-12 10:51:15","updated_at":"2014-12-12 10:51:15"}');
+        $this->mock->shouldReceive('find')->with('1')->once()->andReturn($this->todo);
+        $this->app->instance('\Acme\Todo', $this->mock);
+        $request = $this->call('GET', '/todos/1');
+        $this->assertEquals(200, $request->getStatusCode());
+        $this->assertEquals($request->getContent(), json_encode($this->todo));
     }
 
     public function testGetByNonExistingId()
     {
-        $response = $this->call('GET', '/todos/1010101010101');
-        $this->assertEquals(404, $response->getStatusCode());
-        $this->assertEquals($response->getContent(), '{"error":"Resource Not Found."}');
+        $this->mock->shouldReceive('find')->with('1000000')->once()->andReturn(null);
+        $this->app->instance('\Acme\Todo', $this->mock);
+        $request = $this->call('GET', '/todos/1000000');
+        $this->assertEquals(404, $request->getStatusCode());
+        $this->assertEquals($request->getContent(), '{"error":"Resource Not Found."}');
     }
 
     public function testUpdateExistingResource()
     {
-        $this->call('PUT', '/todos/1', ['title' => 'Update First Todo']);
-        $this->assertTrue($this->client->getResponse()->isOk());
-        $this->assertEquals($this->client->getResponse()->getContent(), '{"id":1,"title":"Update First Todo","completed":1,"created_at":"2014-12-12 10:51:15","updated_at":"2014-12-12 10:51:15"}');
+        $this->update_mock = Mockery::mock('Eloquent', '\Acme\Todo');
+        $this->update_mock->shouldReceive('jsonSerialize')->once();
+        $this->update_mock->shouldReceive('setAttribute')->times(3);
+        $this->update_mock->shouldReceive('save')->once();
+        $this->mock->shouldReceive('find')->with('1')->once()->andReturn($this->update_mock);
+        $this->app->instance('\Acme\Todo', $this->mock);
+        $data    = [
+            'title'     => 'New Todo Item',
+            'completed' => 1
+        ];
+        $request = $this->call('PUT', '/todos/1', $data);
+        $this->assertEquals($request->getContent(), '');
     }
-} 
+
+    public function testUpdateWithNonExistingId()
+    {
+        $this->update_mock = Mockery::mock('Eloquent', '\Acme\Todo');
+        $this->mock->shouldReceive('find')->with('1')->once()->andReturn(null);
+        $this->app->instance('\Acme\Todo', $this->mock);
+        $request = $this->call('PUT', '/todos/1', ['title' => 'Update Todo']);
+        $this->assertEquals($request->getContent(), '{"error":"Resource Not Found."}');
+    }
+
+    public function testDelete()
+    {
+        $this->delete_mock = Mockery::mock('Eloquent', '\Acme\Todo');
+        $this->delete_mock->shouldReceive('jsonSerialize')->once();
+        $this->delete_mock->shouldReceive('delete')->once()->andReturn($this->todo);
+        $this->delete_mock->shouldReceive('find')->with('1')->once()->andReturn($this->delete_mock);
+        $this->app->instance('\Acme\Todo', $this->mock);
+        $request = $this->call('DELETE', '/todos/1');
+        $this->assertEquals($request->getContent(), '', 204);
+    }
+
+}
