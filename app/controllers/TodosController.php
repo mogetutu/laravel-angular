c48fe2b4ada2bac5958d51e0477b48456e86f4ff
mogetutu <imogetutu@gmail.com>
2014-12-12 17:15:53 +0300

    added update functionality

--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -24,7 +24,7 @@ class TodosController extends BaseController {
     public function store()
     {
         $validator = Validator::make(Input::all(), $this->todo->rules());
-        if ($validator->fails()){
+        if ($validator->fails()) {
             return Response::json($validator->messages(), 404);
         }
         $todo = $this->todo->create(['title' => Input::get('title')]);
@@ -40,6 +40,20 @@ class TodosController extends BaseController {
         }
 
         return Response::json(['error' => 'Resource Not Found.'], 404);
+    }
+
+    public function update($id)
+    {
+        $todo = $this->todo->find($id);
+        if (!$todo) return Response::json(['error' => 'Resource Not Found.'], 404);
+
+        $validator = Validator::make(Input::all(), $this->todo->rules());
+        if ($validator->fails()) {
+            return Response::json($validator->messages(), 404);
+        }
+        $todo->update(Input::only('title', 'completed'));
+
+        return Response::json([], 204);
     }
 
 }
