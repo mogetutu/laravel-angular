196e76703b78aaaab7ae095f9df77de8fb51b608
mogetutu <imogetutu@gmail.com>
2014-12-12 18:05:32 +0300

    updated update method

--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -45,15 +45,16 @@ class TodosController extends BaseController {
     public function update($id)
     {
         $todo = $this->todo->find($id);
-        if (!$todo) return Response::json(['error' => 'Resource Not Found.'], 404);
-
+        if (!$todo) {
+            return Response::json(['error' => 'Resource Not Found.'], 404);
+        }
         $validator = Validator::make(Input::all(), $this->todo->rules());
         if ($validator->fails()) {
             return Response::json($validator->messages(), 404);
         }
         $todo->update(Input::only('title', 'completed'));
 
-        return Response::json([], 204);
+        return Response::json('', 204);
     }
 
 }
